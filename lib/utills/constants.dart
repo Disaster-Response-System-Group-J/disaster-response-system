04f78a7bbb46ecf54231bb6ddb4faf94a5f12f75
virusan-T <thavamviru@gmail.com>
2026-05-01 11:00:08 +0530

    add app constrants for database abs statuses

---
+++ lib/utills/constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String databaseName = 'j1_disaster_response.db';
+  static const int databaseVersion = 1;
+
+  static const String eventsTable = 'events';
+
+  static const String statusQueued = 'QUEUED';
+  static const String statusSubmitted = 'SUBMITTED';
+  static const String statusFailed = 'FAILED';
+}
