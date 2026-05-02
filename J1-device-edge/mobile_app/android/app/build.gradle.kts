fdd53a372979e998f0020e4af4a74ac652574850
virusan-T <thavamviru@gmail.com>
2026-05-02 14:15:55 +0530

    set mobile app compile and target SDK to 35

--- J1-device-edge/mobile_app/android/app/build.gradle.kts
+++ J1-device-edge/mobile_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.offline_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     // Use the highest NDK version required by plugins (sqflite_android requires 27.0.12077973)
     ndkVersion = "27.0.12077973"
 
@@ -26,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
