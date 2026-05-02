bfb4eca6f36cda107efe0824482f0d7705fdd7e0
virusan-T <thavamviru@gmail.com>
2026-05-02 10:47:09 +0530

    version,dependance change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.offline_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Use the highest NDK version required by plugins (sqflite_android requires 27.0.12077973)
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
