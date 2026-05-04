29dafefaa126739bc9ea86b99aafc67bb92da289
SudilMin <sudilminthaka8797@gmail.com>
2026-05-04 17:07:59 +0530

    issues fixed

--- J1-device-edge/Central_Node/src/central_node.cpp
+++ J1-device-edge/Central_Node/src/central_node.cpp
@@ -49,11 +49,11 @@ void setup() {
     client.setServer(mqtt_server, 1883);
 
     LoRa.setPins(ss, rst, dio0);
-    if (!LoRa.begin(433E6)) { // Must match Transmitter frequency
+    if (!LoRa.begin(433E6)) {
         Serial.println("Starting LoRa failed!");
         while (1);
     }
-    Serial.println("LoRa Gateway Ready!");
+    Serial.println("LoRa Initialized OK!");
 }
 
 void loop() {
@@ -64,17 +64,37 @@ void loop() {
 
     int packetSize = LoRa.parsePacket();
     if (packetSize) {
-        Serial.print("Received packet: ");
         String receivedData = "";
-        
         while (LoRa.available()) {
             receivedData += (char)LoRa.read();
         }
-        Serial.print(receivedData);
-        Serial.print(" with RSSI ");
-        Serial.println(LoRa.packetRssi());
+        
+        Serial.print("Received packet: ");
+        Serial.println(receivedData);
 
-        // Forward the LoRa packet straight to MQTT broker
-        client.publish("se_project_j/j1/alerts", receivedData.c_str());
+        // Parse JSON to route to correct topic
+        JsonDocument doc;
+        DeserializationError error = deserializeJson(doc, receivedData);
+        
+        if (!error) {
+            String nodeId = doc["node_id"] | "";
+            String type = doc["type"] | "";
+
+            String topic = "";
+            if (nodeId == "J1_TX_01" || type == "SENSOR_DATA" || type == "SENSOR_DATA_FLOOD") {
+                topic = "j1/disaster/flood";
+            } else if (nodeId == "J1_TX_02" || type == "SENSOR_DATA_LANDSLIDE") {
+                topic = "j1/disaster/landslide";
+            }
+
+            if (topic != "") {
+                client.publish(topic.c_str(), receivedData.c_str());
+                Serial.println("Published to topic: " + topic);
+            } else {
+                Serial.println("Unknown node or type. Packet ignored.");
+            }
+        } else {
+            Serial.println("Failed to parse JSON packet");
+        }
     }
 }
--- J1-device-edge/Landside_Node/src/main.cpp
+++ J1-device-edge/Landside_Node/src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include <DHT.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Sensor Pins
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+// LoRa Pins (Standard ESP32 VSPI)
+#define ss 5
+#define rst 14
+#define dio0 26
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_MPU6050 mpu;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println("Starting Landslide Node...");
+
+    dht.begin();
+    
+    // MPU6050 Setup (I2C using default SDA 21, SCL 22 on ESP32)
+    if (!mpu.begin()) {
+        Serial.println("Failed to find MPU6050 chip!");
+    } else {
+        mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+        mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+        mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+        Serial.println("MPU6050 Initialized OK!");
+    }
+
+    LoRa.setPins(ss, rst, dio0);
+    if (!LoRa.begin(433E6)) { // Set your region frequency
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+    Serial.println("LoRa Initialized OK!");
 }
+
+unsigned long lastReadTime = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    if (millis() - lastReadTime >= 2000 || lastReadTime == 0) {
+        lastReadTime = millis();
+        
+        float temp = dht.readTemperature();
+        float hum = dht.readHumidity();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+        // Get new sensor events with the readings
+        sensors_event_t a, g, temp_mpu;
+        mpu.getEvent(&a, &g, &temp_mpu);
+
+        // Prepare JSON
+        JsonDocument doc;
+        doc["node_id"] = "J1_TX_02";
+        doc["type"] = "SENSOR_DATA_LANDSLIDE";
+        
+        if (isnan(temp) || isnan(hum)) {
+            doc["temperature"] = serialized("null");
+            doc["humidity"] = serialized("null");
+        } else {
+            doc["temperature"] = temp;
+            doc["humidity"] = hum;
+        }
+
+        doc["accel_x"] = a.acceleration.x;
+        doc["accel_y"] = a.acceleration.y;
+        doc["accel_z"] = a.acceleration.z;
+        doc["gyro_x"] = g.gyro.x;
+        doc["gyro_y"] = g.gyro.y;
+        doc["gyro_z"] = g.gyro.z;
+
+        String jsonString;
+        serializeJson(doc, jsonString);
+
+        Serial.println("--- LANDSLIDE NODE READINGS ---");
+        Serial.print("JSON Output: "); Serial.println(jsonString);
+
+        // Send packet
+        LoRa.beginPacket();
+        LoRa.print(jsonString);
+        LoRa.endPacket();
+    }
 }
