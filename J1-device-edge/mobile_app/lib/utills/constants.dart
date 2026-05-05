bd83a27528132b5ebd6f500280cfc27798500f2f
virusan-T <thavamviru@gmail.com>
2026-05-05 16:51:54 +0530

    update constants

--- J1-device-edge/mobile_app/lib/utills/constants.dart
+++ J1-device-edge/mobile_app/lib/utills/constants.dart
@@ -3,6 +3,12 @@ class AppConstants {
   static const int databaseVersion = 1;
 
   static const String eventsTable = 'events';
+  static const String usersTable = 'users';
+  static const String incomingReportsTable = 'incoming_reports';
+  static const String confirmedIncidentsTable = 'confirmed_incidents';
+  static const String resourcesTable = 'resources';
+  static const String alertsTable = 'alerts';
+  static const String syncMetadataTable = 'sync_metadata';
 
   static const String statusQueued = 'QUEUED';
   static const String statusSubmitted = 'SUBMITTED';
